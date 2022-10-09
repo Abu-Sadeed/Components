9c3c4bc2788ee57d141f55a956c48ae912a7fe69
Sadeed <abusadeed@iut-dhaka.edu>
2022-10-09 18:57:59 +0600

    built basic pop-up

---
+++ Modal/app.js
@@ -0,0 +1,11 @@
+var openBtn = document.querySelector('.open');
+var modalContainer = document.querySelector('.modal-container');
+var modalBtn = document.querySelector('.modal-btn');
+
+openBtn.addEventListener('click', function() {
+    modalContainer.classList.add('show');
+});
+
+modalBtn.addEventListener('click', function() {
+    modalContainer.classList.remove('show');
+});
