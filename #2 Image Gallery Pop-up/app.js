8c54ea9b581ef63a22b758e86d8fe8db0fe526e1
Sadeed <abusadeed@iut-dhaka.edu>
2022-10-23 11:04:19 +0600

    changed code format

--- #2 Image Gallery Pop-up/app.js
+++ #2 Image Gallery Pop-up/app.js
@@ -1,22 +1,21 @@
-const fullImg = document.querySelector('.full-img');
-const smallImg = document.querySelectorAll('.gallery img');
+const fullImg = document.querySelector(".full-img");
+const smallImg = document.querySelectorAll(".gallery img");
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector(".modal");
 
-smallImg.forEach(element => {
-    element.onclick = () => {
-        modal.classList.add('open');
-        fullImg.classList.add('open');
+smallImg.forEach((element) => {
+  element.onclick = () => {
+    modal.classList.add("open");
+    fullImg.classList.add("open");
 
-        const originalQuality = element.getAttribute('alt');
-        fullImg.src = `img/full/${originalQuality}.jpg`;
-    }
-
+    const originalQuality = element.getAttribute("alt");
+    fullImg.src = `img/full/${originalQuality}.jpg`;
+  };
 });
 
 modal.onclick = (e) => {
-    if(e.target.classList.contains('modal')) {
-        modal.classList.remove('open');
-        fullImg.classList.remove('open');
-    }
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    fullImg.classList.remove("open");
+  }
 };
--- #3 NavBar Effect/app.js
+++ #3 NavBar Effect/app.js
@@ -1,39 +1,38 @@
-const sections = document.querySelectorAll('section');
-const trans = document.querySelector('.trans');
-const gradients = ['coral', 'chartreuse', 'chocolate', 'cadetblue'];
+const sections = document.querySelectorAll("section");
+const trans = document.querySelector(".trans");
+const gradients = ["coral", "chartreuse", "chocolate", "cadetblue"];
 
 const options = {
-    threshold: 0.7,
+  threshold: 0.7,
 };
 
 let observer = new IntersectionObserver(navScroll, options);
 
 function navScroll(entries) {
-    entries.forEach(element => {
-        const className = element.target.className;
-        const activeLink = document.querySelector(`[data-page="${className}"]`);
-        const elementIndex = element.target.getAttribute('data-index');
-        
+  entries.forEach((element) => {
+    const className = element.target.className;
+    const activeLink = document.querySelector(`[data-page="${className}"]`);
+    const elementIndex = element.target.getAttribute("data-index");
 
-        const coords = activeLink.getBoundingClientRect();
+    const coords = activeLink.getBoundingClientRect();
 
-        const directions = {
-            height: coords.height,
-            width: coords.width,
-            top: coords.top,
-            left: coords.left,
-        };
-    
-        if(element.isIntersecting){
-            trans.style.setProperty('height', `${directions.height}px`);
-            trans.style.setProperty('width', `${directions.width}px`);
-            trans.style.setProperty('top', `${directions.top}px`);
-            trans.style.setProperty('left', `${directions.left}px`);
-            trans.style.backgroundColor = gradients[elementIndex];
-        }
-    });
-};
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left,
+    };
 
-sections.forEach(section => {
-    observer.observe(section);
+    if (element.isIntersecting) {
+      trans.style.setProperty("height", `${directions.height}px`);
+      trans.style.setProperty("width", `${directions.width}px`);
+      trans.style.setProperty("top", `${directions.top}px`);
+      trans.style.setProperty("left", `${directions.left}px`);
+      trans.style.backgroundColor = gradients[elementIndex];
+    }
+  });
+}
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
