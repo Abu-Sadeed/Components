c8dac002182c2e9aea01227e0341de9f1f5ffc84
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-02 20:26:53 +0600

    Buttons are hidden based on image position

--- #10 Gallery Carousel/app.js
+++ #10 Gallery Carousel/app.js
@@ -16,26 +16,34 @@ const imageSlider = (firstImg, secondImg) => {
   list.style.transform = "translateX(-" + secondImg.style.left + ")";
 };
 
+//Hide slide buttons
+const hideButton = (index) => {
+  if (index === 0) {
+    leftBtn.classList.add("hidden");
+    rightBtn.classList.remove("hidden");
+  } else if (index === imgs.length - 1) {
+    rightBtn.classList.add("hidden");
+    leftBtn.classList.remove("hidden");
+  } else {
+    leftBtn.classList.remove("hidden");
+    rightBtn.classList.remove("hidden");
+  }
+};
+
 imgs.forEach(setImgPosition);
 
 rightBtn.addEventListener("click", () => {
   const currentImage = list.querySelector(".current-img");
-  const currentIndex = imgs.indexOf(currentImage);
-  if (currentIndex === imgs.length - 1) {
-    alert("You are at the end of the gallery");
-  } else {
-    const nextImage = currentImage.nextElementSibling;
-    imageSlider(currentImage, nextImage);
-  }
+  const nextImage = currentImage.nextElementSibling;
+  const currentIndex = imgs.indexOf(nextImage);
+  hideButton(currentIndex);
+  imageSlider(currentImage, nextImage);
 });
 
 leftBtn.addEventListener("click", () => {
   const currentImage = list.querySelector(".current-img");
-  const currentIndex = imgs.indexOf(currentImage);
-  if (currentIndex === 0) {
-    alert("You are at the beginning of the gallery");
-  } else {
-    const prevImage = currentImage.previousElementSibling;
-    imageSlider(currentImage, prevImage);
-  }
+  const prevImage = currentImage.previousElementSibling;
+  const currentIndex = imgs.indexOf(prevImage);
+  hideButton(currentIndex);
+  imageSlider(currentImage, prevImage);
 });
