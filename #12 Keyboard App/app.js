a3eb5dbae6320fd991c598967c8db0ae31743c17
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-05 11:24:39 +0600

    Added alphabet case changing functionanlity

--- #12 Keyboard App/app.js
+++ #12 Keyboard App/app.js
@@ -1,6 +1,20 @@
 const keyContainers = document.querySelectorAll(".keys");
 let textArea = document.querySelector(".display textarea");
+let capsLock = false;
+const alphabetCase = document.querySelector(".alphabet-case");
 
+// Change uppercase and lowercase symbol
+const changeCase = () => {
+  if (capsLock) {
+    alphabetCase.value = "⇪";
+    capsLock = false;
+  } else {
+    alphabetCase.value = "⬆";
+    capsLock = true;
+  }
+};
+
+// Add event listener to each key
 keyContainers.forEach((keyContainer) => {
   keyContainer.addEventListener("click", (e) => {
     if (!e.path[0].value) return;
@@ -9,6 +23,14 @@ keyContainers.forEach((keyContainer) => {
       return;
     }
 
-    textArea.value += e.path[0].value;
+    if (e.path[0].className === "alphabet-case") {
+      changeCase();
+      return;
+    }
+    if (capsLock) {
+      textArea.value += e.path[0].value;
+      return;
+    }
+    textArea.value += e.path[0].value.toLowerCase();
   });
 });
