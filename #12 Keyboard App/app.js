902276c4bd4f805857e96cdb29da97f22e573513
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-05 09:18:10 +0600

    added backspace

--- #12 Keyboard App/app.js
+++ #12 Keyboard App/app.js
@@ -4,6 +4,11 @@ let textArea = document.querySelector(".display textarea");
 keyContainers.forEach((keyContainer) => {
   keyContainer.addEventListener("click", (e) => {
     if (!e.path[0].value) return;
+    if (e.path[0].value === "⌫") {
+      textArea.value = textArea.value.slice(0, -1);
+      return;
+    }
+
     textArea.value += e.path[0].value;
   });
 });
