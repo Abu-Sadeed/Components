8bc4fcd250bd3644da874757b76aa3f3d858b1f8
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-09 07:20:26 +0600

    added shape shifting

--- #14 CSS Property Changer/app.js
+++ #14 CSS Property Changer/app.js
@@ -2,6 +2,8 @@ const item = document.querySelector("#block");
 const vertical = document.querySelector("#position-y");
 const horizontal = document.querySelector("#position-x");
 const size = document.querySelector("#size");
+const shapeSelect = document.querySelector("#shape-select");
+const shapeBtn = document.querySelector("#ok-btn");
 
 // Positioning
 vertical.addEventListener("change", function () {
@@ -15,4 +17,14 @@ horizontal.addEventListener("change", function () {
 // Size
 size.addEventListener("change", function () {
   item.style.transform = "scale(" + size.value + ")";
+});
+
+// Shape
+shapeBtn.addEventListener("click", function () {
+  let shape = shapeSelect.value;
+  if (shape === "1") {
+    item.style.borderRadius = "0";
+  } else if (shape === "2") {
+    item.style.borderRadius = "50%";
+  }
 });
