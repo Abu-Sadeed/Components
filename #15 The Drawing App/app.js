a856ee2e3bcdc8573819d8ce9222cd40b420de1f
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-10 06:37:06 +0600

    stroke width can be increaseed or decreased

--- #15 The Drawing App/app.js
+++ #15 The Drawing App/app.js
@@ -57,3 +57,24 @@ const drawingCircles = (x, y) => {
   ctx.fillStyle = color;
   ctx.fill();
 };
+
+//increase size
+const updateSizeOnScreen = () => {
+  strokeSize.innerText = size;
+};
+
+increaseBtn.addEventListener("click", () => {
+  size += 5;
+  if (size > 50) {
+    size = 50;
+  }
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size -= 5;
+  if (size < 5) {
+    size = 5;
+  }
+  updateSizeOnScreen();
+});
