b59879427d75e4deec154fc3a4e2719d70020632
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-11 12:08:28 +0600

    Added pressdown effect

--- #16 Piano App/app.js
+++ #16 Piano App/app.js
@@ -2,7 +2,13 @@ const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
   key.addEventListener("click", () => {
+    key.classList.add("active");
     console.log(key.dataset.note);
-    document.getElementById(key.dataset.note).play();
+    const audio = document.getElementById(key.dataset.note);
+    audio.currentTime = 0;
+    audio.play();
+    audio.addEventListener("ended", () => {
+      key.classList.remove("active");
+    });
   });
 });
