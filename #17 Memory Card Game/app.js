086fec21c5d896a3dc11f27751603be6b833b979
Sadeed <abusadeed@iut-dhaka.edu>
2022-11-14 07:13:38 +0600

    refractored and set board locked

--- #17 Memory Card Game/app.js
+++ #17 Memory Card Game/app.js
@@ -1,27 +1,41 @@
 const memoryCard = document.querySelectorAll(".memory-card");
 let cardIsFlipped = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 const flipCard = function () {
+  if (lockBoard) return;
   this.classList.toggle("flip");
 
   if (!cardIsFlipped) {
     cardIsFlipped = true;
     firstCard = this;
-  } else {
-    cardIsFlipped = false;
-    secondCard = this;
-
-    if (firstCard.dataset.name === secondCard.dataset.name) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    } else {
-      setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-      }, 1000);
-    }
+    return;
   }
+
+  secondCard = this;
+
+  matchCheck();
+};
+
+const matchCheck = function () {
+  if (firstCard.dataset.name === secondCard.dataset.name) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetBoard();
+  } else {
+    lockBoard = true;
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      resetBoard();
+    }, 1000);
+  }
+};
+
+const resetBoard = function () {
+  [cardIsFlipped, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 };
 
 memoryCard.forEach((card) => card.addEventListener("click", flipCard));
